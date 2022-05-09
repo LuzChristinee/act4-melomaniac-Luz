a061ce046bbb7d781d53718a1c8955f43ca86030
LuzChristine <iamluzchristinee@gmail.com>
2022-05-09 16:49:50 +0800

    Song2

---
+++ Song2.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> SMART RECORDS </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to SMART RECORDS</h1>
+                <p class="lead">“Keep a song in your heart here in SMART RECORDS.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Super Far</h2>";
+                        echo "<p class='lead '>Lany</p>";
+                        echo "<br/>";
+                
+                        echo "<p> [Verse 1]</p>";
+                        echo "<p> Up all night on my mind got me thinking</p>";
+                        echo "<p> Wanna stay, can you give me a reason?</p>";
+                        echo "<p> I don't think so, I don't think so</p>";
+                        echo "<p> I'm in love with someone, but I'm not sure</p>";
+                        echo "<p> She can love someone back the way they love her</p>";
+                        echo "<p> I don't think so, I don't think so</p>";
+                        echo "<br/>";
+
+
+                        //Pre-Chorus
+                        echo "<p> [Pre-Chorus]</p>";
+                        echo "<p> Don't be mean, if you wanna go</p>";
+                        echo "<p> You can leave and leave my heart alone</p>";
+                        echo "<br/>";
+
+
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Waking up to nothing when you're super far from home</p>";
+                        echo "<p> And I watch you fall asleep at night and lay there on my own</p>";
+                        echo "<p> Got me begging for affection, all you do is roll your eyes</p>";
+                        echo "<p> Broken down, I've had enough?</p>";
+                        echo "<p> If this is love, I don't want it</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> [Verse 2]</p>";
+                        echo "<p> Give it time, c'mon babe, it's been ten months</p>";
+                        echo "<p> You should know, do you know, know what you want?</p>";
+                        echo "<p> I don't think so, I don't think so</p>";
+                        echo "<p> Make it work, know it hurts, but I'll go there</p>";
+                        echo "<p> Do the same, can you show me that you care?</p>";
+                        echo "<p> God, I hope so, but I don't think so</p>";
+                        echo "<br/>";
+
+                        //Pre-Chorus
+                        echo "<p> [Pre-Chorus]</p>";
+                        echo "<p> Don't be mean, if you're here for good</p>";
+                        echo "<p> I'm gonna need a little more from you</p>";
+                        echo "<br/>";
+
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Waking up to nothing when you're super far from home</p>";
+                        echo "<p> And I watch you fall asleep at night and lay there on my own</p>";
+                        echo "<p> Got me begging for affection, all you do is roll your eyes</p>";
+                        echo "<p> Broken down, I've had enough?</p>";
+                        echo "<p> If this is love, I don't want it</p>";
+                        echo "<p> All my friends keep saying that I'm way too good to you</p>";
+                        echo "<p> But my heart is so invested, I don't wanna face the truth</p>";
+                        echo "<p> I'm not happy, and you know it, and you still don't even try</p>";
+                        echo "<p> Broken down, I've had enough</p>";
+                        echo "<p> If this is love, I don't want it</p>";
+                        echo "<br/>";
+
+                        echo "<p> [Instrumental]<p/>";
+
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Waking up to nothing when you're super far from home</p>";
+                        echo "<p> And I watch you fall asleep at night and lay there on my own</p>";
+                        echo "<p> Got me begging for affection, all you do is roll your eyes</p>";
+                        echo "<p> Broken down, I've had enough?</p>";
+                        echo "<p> If this is love, I don't want it</p>";
+                        echo "<p> All my friends keep saying that I'm way too good to you</p>";
+                        echo "<p> But my heart is so invested, I don't wanna face the truth</p>";
+                        echo "<p> I'm not happy, and you know it, and you still don't even try</p>";
+                        echo "<p> Broken down, I've had enough</p>";
+                        echo "<p> If this is love, I don't want it</p>";
+                        echo "<br/>";
+
+                    
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Christine Luz </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
